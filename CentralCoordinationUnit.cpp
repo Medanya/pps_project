13997a7e1a02bc0599dc10fe60d01c853f777843
vlad <vvladxx@gmail.com>
2016-12-05 00:31:02 +0300

    Roses are blue, Violets are red, I'm no good at colours, But I'm brilliant in bed

--- CentralCoordinationUnit.cpp
+++ CentralCoordinationUnit.cpp
@@ -3,7 +3,56 @@
 CentralCoordinationUnit CentralCoordinationUnit::instance;
 
 void CentralCoordinationUnit::handleEvent(Event event) {
-	
+	/*switch (event) {
+		case TRAIN_COMING: {
+			trainComing = true;
+			unsigned i = 0;
+			for (; i < kNumberOfRailways; ++i)
+				if (vehicleOnCrossing[i])
+					break;
+			if (i < kNumberOfRailways) {
+				
+			}
+			break;
+		}
+		case TRAIN_LEAVING:
+			trainComing = false;
+			for (BoomBarrier *barrier : boomBarrierOut)
+				barrier->setBoomBarrierState(true);
+			for (BoomBarrier *barrier : boomBarrierIn)
+				barrier->setBoomBarrierState(true);
+			break;
+		case VEHICLE_ON_CROSSING:
+			vehicleOnCrossing = true;
+			if (trainComing) {
+				for (BoomBarrier *barrier : boomBarrierOut)
+					barrier->setBoomBarrierState(true);
+				for (BoomBarrier *barrier : boomBarrierIn)
+					barrier->setBoomBarrierState(false);
+			}
+			else {
+				for (BoomBarrier *barrier : boomBarrierOut)
+					barrier->setBoomBarrierState(true);
+				for (BoomBarrier *barrier : boomBarrierIn)
+					barrier->setBoomBarrierState(true);
+			}
+			break;
+		case VEHICLE_LEAVING:
+			vehicleOnCrossing = false;
+			if (trainComing) {
+				for (BoomBarrier *barrier : boomBarrierOut)
+					barrier->setBoomBarrierState(false);
+				for (BoomBarrier *barrier : boomBarrierIn)
+					barrier->setBoomBarrierState(false);
+			}
+			else {
+				for (BoomBarrier *barrier : boomBarrierOut)
+					barrier->setBoomBarrierState(true);
+				for (BoomBarrier *barrier : boomBarrierIn)
+					barrier->setBoomBarrierState(true);
+			}
+			break;
+	}*/
 }
 
 void CentralCoordinationUnit::eventLoop() {
